30f7a6ae5be99643d1ca6e74966ea37cf9c3a70f
notvikke <97vigneshvn@student.ie.edu>
2026-02-03 20:56:04 +0530

    debug: Hardcode IDs to rule out env var issues

--- frontend/lib/amplify.ts
+++ frontend/lib/amplify.ts
@@ -5,9 +5,9 @@ import { Amplify } from 'aws-amplify';
 Amplify.configure({
     Auth: {
         Cognito: {
-            userPoolId: process.env.NEXT_PUBLIC_USER_POOL_ID!,
-            userPoolClientId: process.env.NEXT_PUBLIC_USER_POOL_CLIENT_ID!,
-            identityPoolId: process.env.NEXT_PUBLIC_IDENTITY_POOL_ID!,
+            userPoolId: 'us-east-1_Us8h3nB9q',
+            userPoolClientId: '4ukbepn8u9tcio912lrabv6vf0',
+            identityPoolId: 'us-east-1:d930a49c-60b7-4192-8197-8011ce5994f6',
         }
     }
 });
